7d9de03f5b4d372a414f4eff46adfdddd8aaeff8
nameslessjin <42518630+nameslessjin@users.noreply.github.com>
2023-12-19 15:02:51 -0800

    Advance lighting

---
+++ 29.AdvancedLighting/AdvancedLighting/AdvancedLighting/AdvancedLightingApp.swift
@@ -0,0 +1,18 @@
+//
+//  AdvancedLightingApp.swift
+//  AdvancedLighting
+//
+//  Created by JINSEN WU on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedLightingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .navigationTitle("Advanced Lighting")
+        }
+    }
+}
