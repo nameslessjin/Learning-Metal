7541f9208006e0308d15e84ca5696ea7762aaac0
nameslessjin <42518630+nameslessjin@users.noreply.github.com>
2023-12-20 15:40:24 -0800

    Performance Optimization

---
+++ 31.PerformanceOptimization/PerformanceOptimization/PerformanceOptimization/PerformanceOptimizationApp.swift
@@ -0,0 +1,18 @@
+//
+//  PerformanceOptimizationApp.swift
+//  PerformanceOptimization
+//
+//  Created by JINSEN WU on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PerformanceOptimizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .navigationTitle("Performance Optimization")
+        }
+    }
+}
